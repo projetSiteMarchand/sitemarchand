8ff17c870033818e3cc2f8ab74ee1949df5ff5e7
statler <statler@statler.fr>
2013-03-14 02:53:22 +0100

    Ajout page de déconnexion

---
+++ modules/membres/deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+defined('ALLOWED') or die();
+if($membre = Membre::connecte() && !empty($_GET['token']) && $_GET['token'] == $_SESSION['token'])
+{
+	$membre->deconnecter();
+	redirect(SITE);
+}
+else
+{
+	include ERREURS.'page-introuvable.php';
+}
+?>
