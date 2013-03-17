0e8fcee8b37f2c5b876f8045960d0aadb046f367
statler <statler@statler.fr>
2013-03-17 20:34:52 +0100

    Ajout des classes Enchere et Evaluation

---
+++ includes/classes/enchere.class.php
@@ -0,0 +1,13 @@
+<?php
+class Enchere
+{
+    public static $nomTable = 'ENCHERE';
+
+    private $idEnchere;
+    private $montantEnchere;
+    private $dateEnchere;
+    private $evaluationVendeur;
+    private $evaluationAcheteur;
+    private $produitEnchere;
+}
+?>
---
+++ includes/classes/evaluation.class.php
@@ -0,0 +1,13 @@
+<?php
+class Evaluation 
+{
+    public static $nomTable = 'EVALUATION';
+    public static $maxNote = 5;
+    public static $minNote = 1;
+
+    private $idEvaluation;
+    private $membre;
+    private $note;
+    private $commentaire;
+}
+?>
